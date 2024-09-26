a76039f7bee1d2a6ec967053db945bd4afda9eeb
ToniEstarlich <110102891+ToniEstarlich@users.noreply.github.com>
2024-09-26 18:25:41 +0100

    connect Header to Home

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Navbar from "../Navbar/Navbar";
+import "./Header.css";
+
+const Header = () => {
+    return(
+        <>
+            <Navbar />
+            <h1>I'm the header</h1>
+        </>
+    
+    )
+}
+
+export default Header 
